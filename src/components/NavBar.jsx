5a1f4d43febef14ad735322e93178131a207c9b2
marcia-gmarques <marciagm00@gmail.com>
2025-03-09 21:22:04 +0100

    assets

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='flex justify-center w-full'>
-        <ul className='flex space-x-6 p-4'>
+        <ul className='flex space-x-6 p-4 text-[#bdebfb]'>
             <li className='p-4'>Content</li>
             <li className='p-4'>Play-through</li>
             <li className='p-4'>Our Team</li>
