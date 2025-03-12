4917eaeb4b6ff41a9e84f1e5b1d0b91acf2afe26
marcia-gmarques <marciagm00@gmail.com>
2025-03-12 10:16:44 +0100

    assets and font

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/NavBar.css';
 
 const NavBar = () => {
   return (
-    <div className='flex justify-center w-full'>
+    <div className='flex justify-center w-full font-bovine sticky'>
         <ul className='flex space-x-6 p-4 text-[#bdebfb]'>
             <li className='p-4'>Content</li>
             <li className='p-4'>Play-through</li>
