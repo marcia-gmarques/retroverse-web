d4008689d96ea3bf179bbca9302e0d2659f78a8f
marcia-gmarques <marciagm00@gmail.com>
2025-09-05 15:24:27 +0200

    spline ghost positioning

--- src/components/SplineGhost.jsx
+++ src/components/SplineGhost.jsx
@@ -1,8 +1,9 @@
 import Spline from '@splinetool/react-spline';
+import '../styles/SplineGhost.css';
 
 export default function SplineGhost() {
   return (
-    <main>
+    <main className="spline-ghost">
       <Spline
         scene="https://prod.spline.design/2QosnLePckaMuVGh/scene.splinecode" 
       />
